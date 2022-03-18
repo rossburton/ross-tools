5187f81555b6393b605e021a28c2a168062947db
Ross Burton <ross.burton@arm.com>
2022-03-18 11:19:19 +0000

    gitlab-job-cancel: add --use-config to allow settings in .git/config

--- gitlab-job-cancel.py
+++ gitlab-job-cancel.py
@@ -8,16 +8,30 @@ import gitlab
 
 parser = argparse.ArgumentParser()
 parser.add_argument("--server", nargs="?")
-parser.add_argument("--project", "-p", required=True)
+parser.add_argument("--project", "-p")
 parser.add_argument("--pipeline", "-l", required=True, help="pipeline ID, or branch name for latest pipeline")
 parser.add_argument("--stage", "-s", nargs="?")
 parser.add_argument("--keep", "-k", nargs="?")
 # TODO make keep exclusive with --cancel
 parser.add_argument("--dry-run", action="store_true")
+parser.add_argument("--use-config", "-c", action="store_true")
+
 args = parser.parse_args()
 
-# TODO config file support (--config) which will read config for the arguments.
-# etc meta-arm can set project/server/stage.
+# If we're using a configuration, load it into args and re-parse to override the settings
+if args.use_config:
+    import configparser, git
+    repo = git.Repo(search_parent_directories=True)
+    with repo.config_reader() as config:
+        for key in ("project", "stage"):
+            try:
+                value = config.get("ross-tools", key)
+                setattr(args, key, value)
+            except configparser.NoOptionError:
+                pass
+    args = parser.parse_args(namespace=args)
+
+# TODO check variables are set
 
 gl = gitlab.Gitlab.from_config(args.server)
 gl.auth()
