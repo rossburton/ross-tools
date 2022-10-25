09f9a57d8f030dc4f9ae3488858407b47e37c537
Ross Burton <ross.burton@arm.com>
2022-10-25 14:59:28 +0100

    Add tool to fix groups.io author munging

---
+++ authormunger.py
@@ -0,0 +1,37 @@
+#! /usr/bin/env python3
+
+"""
+When groups.io can't agree who is sending an email for SMTP being horrible
+reasons, you end up with patches with bad authorship:
+
+Author: wangmy via lists.openembedded.org
+<wangmy=fujitsu.com@lists.openembedded.org>
+
+Extract the email address, find a matching S-o-b, and use that name/email as the
+correct author.
+"""
+
+# SPDX-License-Identifier: MIT
+
+import pygit2
+import os
+import email.utils
+
+path = pygit2.discover_repository(os.getcwd())
+repo = pygit2.Repository(path)
+commit = repo.revparse_single(repo.head.name)
+
+old_sig = commit.author
+if "via lists.openembedded.org" in old_sig.name:
+    target_email = old_sig.email.split("@")[0].replace("=", "@")
+    print(f"Looking for {target_email}")
+    # Can't use commit.message_trailers as that is a dict so doesn't preserve
+    # multiple S-o-b (https://github.com/libgit2/pygit2/issues/1170)
+    for line in commit.message.splitlines():
+        if "Signed-off-by" in line and target_email in line:
+            addr = line.split(":")[1].strip()
+            name, email = email.utils.parseaddr(addr)
+
+            signature = pygit2.Signature(name, email, old_sig.time, old_sig.offset)
+            repo.amend_commit(commit, repo.head.name, author=signature)
+            break
